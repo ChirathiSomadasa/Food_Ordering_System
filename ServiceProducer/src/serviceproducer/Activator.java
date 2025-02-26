4e0f1957cd745477f111af2850bc624684ecc47e
Udeshika Balasooriya <159142506+UdeshikaP@users.noreply.github.com>
2025-02-26 23:56:52 +0530

    create service producer

--- ServiceProducer/src/serviceproducer/Activator.java
+++ ServiceProducer/src/serviceproducer/Activator.java
@@ -2,17 +2,29 @@ package serviceproducer;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
+	private ServiceRegistration customerServiceRegistration;
+
 	@Override
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Hello World!!");
+		System.out.println(".....Producer Services started......");
+
+		// create an object of customer service
+		ICustomerService customerService = new CustomerService();
+
+		// register the service with the OSGI framework
+		customerServiceRegistration = context.registerService(ICustomerService.class.getName(), customerService, null);
 	}
-	
+
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		System.out.println("Goodbye World!!");
+		System.out.println(".....Producer Services stopped......");
+
+		// unregister the customer service before the stopping the bundle
+		customerServiceRegistration.unregister();
 	}
 
 }
---
+++ ServiceProducer/src/serviceproducer/ICustomerService.java
@@ -0,0 +1,16 @@
+package serviceproducer;
+
+/**
+ * The ICustomerService interface defines the contact for customer related
+ * service Implementing classes should provides methods to display the customer
+ * details and send notifications
+ */
+public interface ICustomerService {
+
+	// display customer details such as name,email,address,contact
+	void displayCustomerDetails(String name, String email, String address, String contact);
+
+	// Sends notification message to the customer
+	void notifyCustomer(String message);
+
+}
