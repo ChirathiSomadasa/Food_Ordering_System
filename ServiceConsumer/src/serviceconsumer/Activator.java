2e17160bc03b9be4056ef38dbda8d7d7dc684ceb
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-02-27 21:03:18 +0530

    Update Producer and Consumer Activator

--- ServiceConsumer/src/serviceconsumer/Activator.java
+++ ServiceConsumer/src/serviceconsumer/Activator.java
@@ -12,13 +12,10 @@ import serviceproducer.IDeliveryService;
 
 public class Activator implements BundleActivator {
 	
-
-	private ServiceReference deliveryServiceReference;
-
 	private ServiceReference foodMenuServiceReference;
-
-	private ServiceReference paymentServiceReference;
 	private ServiceReference customerServiceReference;
+	private ServiceReference deliveryServiceReference;
+	private ServiceReference paymentServiceReference;
 
 	@Override
 	public void start(BundleContext context) throws Exception {
@@ -27,39 +24,28 @@ public class Activator implements BundleActivator {
 		
 		//Get references to the producer services
 		
-		deliveryServiceReference = context.getServiceReference(IDeliveryService.class.getName());
-		paymentServiceReference = context.getServiceReference(IPaymentService.class.getName());
 		foodMenuServiceReference = context.getServiceReference(IFoodMenu.class.getName());
 		paymentServiceReference = context.getServiceReference(IPaymentService.class.getName());
+		deliveryServiceReference = context.getServiceReference(IDeliveryService.class.getName());
 		
 		IFoodMenu foodMenuService = (IFoodMenu) context.getService(foodMenuServiceReference);
 		IPaymentService paymentService = (IPaymentService) context.getService(paymentServiceReference);
 		IDeliveryService deliveryService = (IDeliveryService) context.getService(deliveryServiceReference);
-		RestaurantSystem restaurantSystem = new RestaurantSystem(deliveryService);
-		//PaymentSystem paymentSystem = new PaymentSystem(paymentService,foodMenuService);
-		//paymentSystem.processPayment("Pizza", 2, "Credit Card", foodMenuService, "Credit Card");
-		ICustomerService customerService = (ICustomerService) context.getService(customerServiceReference);
 
-		
 		// Initialize consumer services
-
-
-		
-
-		// Create an object of CustomerSystem using the retrieved customerService
-
+		RestaurantSystem restaurantSystem = new RestaurantSystem(deliveryService);
+		PaymentSystem paymentSystem = new PaymentSystem(paymentService,foodMenuService);
+		ICustomerService customerService = (ICustomerService) context.getService(customerServiceReference);
 		OrderProcessor orderProcessor = new OrderProcessor(foodMenuService);
-		CustomerSystem customerSystem = new CustomerSystem(customerService);
-		// Create an object of CustomerSystem using the retrieved customerService
+		CustomerSystem customerSystem = new CustomerSystem(customerService);// Create an object of CustomerSystem using the retrieved customerService
 
+		//sample data
 		orderProcessor.placeOrder("Pizza", 2);
 		orderProcessor.processOrder("Pizza", 2);
 		paymentSystem.processPayment("Pizza", 2, "Credit Card", foodMenuService, "Credit Card");
 		customerSystem.inputCustomerDetails("Amali", "amali@gmail.com", "Kurunegala", "0110000000");
 		restaurantSystem.notifyCustomer("12345","delivered");
-		// Provide notification
-
-		customerSystem.notifyCustomer("Order is comfirmed");
+		customerSystem.notifyCustomer("Order is comfirmed");// Provide notification
 
 	}
 
@@ -68,10 +54,10 @@ public class Activator implements BundleActivator {
 
 		System.out.println("Consumer services stopped");
 
-		context.ungetService(deliveryServiceReference);
 		context.ungetService(foodMenuServiceReference);
 		context.ungetService(customerServiceReference);
 		context.ungetService(paymentServiceReference);
+		context.ungetService(deliveryServiceReference);
 
 	}
 
--- ServiceProducer/src/serviceproducer/Activator.java
+++ ServiceProducer/src/serviceproducer/Activator.java
@@ -16,34 +16,22 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext context) throws Exception {
 		System.out.println(".....Producer Services started.....");
 
-
-		
-
-		// register the service with the OSGI framework		
-		CustomerService customerService = new CustomerService(null);
-
-
-		
-
-		// register the service with the OSGI framework
-		customerServiceRegistration = context.registerService(ICustomerService.class.getName(), customerService, null);
-		
-
-		customerServiceRegistration = context.registerService(
-				ICustomerService.class.getName(), customerService, null);
-
-	
 		RestaurantService restaurantService = new RestaurantService();
 		foodMenuReServiceRegistration = context.registerService(
 				IFoodMenu.class.getName(), restaurantService, null);
+		
+		CustomerService customerService = new CustomerService(null);
+		customerServiceRegistration = context.registerService(
+				ICustomerService.class.getName(), customerService, null);
+		
+		PaymentService paymentService = new PaymentService();
+		paymentServiceRegistration = context.registerService(
+				IPaymentService.class.getName(), paymentService, null);
 		
 		DeliveryService deliveryService = new DeliveryService(customerService);
 		deliveryServiceRegistration = context.registerService(
 				IDeliveryService.class.getName(), deliveryService, null);
 		
-		PaymentService paymentService = new PaymentService();
-		paymentServiceRegistration = context.registerService(
-				IPaymentService.class.getName(), paymentService, null);
 
 	}
 
@@ -52,10 +40,11 @@ public class Activator implements BundleActivator {
 
 		System.out.println(".....Producer Services stopped.....");
 
-		customerServiceRegistration.unregister();		
 		foodMenuReServiceRegistration.unregister();
-		deliveryServiceRegistration.unregister();
+		customerServiceRegistration.unregister();		
 		paymentServiceRegistration.unregister();
+		deliveryServiceRegistration.unregister();
+		
 
 	}
 
