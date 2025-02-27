196344370f9127c25ebefd6b8cd2cf70b952f4a4
IT22309624 <129936797+IT22309624@users.noreply.github.com>
2025-02-28 01:59:33 +0530

    update menu

--- ServiceConsumer/src/serviceconsumer/OrderProcessor.java
+++ ServiceConsumer/src/serviceconsumer/OrderProcessor.java
@@ -2,26 +2,33 @@ package serviceconsumer;
 
 import serviceproducer.ICustomerService;
 import serviceproducer.IFoodMenu;
+import serviceproducer.IPaymentService;
 
 public class OrderProcessor {
 	
 	private IFoodMenu menuService;
 	private ICustomerService customerService;
+	private IPaymentService paymentService;
 
 
 	public OrderProcessor(IFoodMenu menuService) {
 		this.menuService = menuService;
+//		this.paymentService = paymentService;
 //		this.customerService = customerService;
 	}
 	
 	public void placeOrder(String item, int qty) {
 		menuService.displayMenu();
-		System.out.println("Order place : " + item + " x " + qty );
+		System.out.println("\nPlace order : " + item + " x " + qty );
 		
 
 	}
 	public void processOrder(String item, int qty) {
-		menuService.processOrder(item, qty);
+		
+		double itemPrice = menuService.getPrice(item);
+		double totalAmount = itemPrice * qty;
+		
+		menuService.processOrder(item, qty, totalAmount);
 	}
 	
 //	public void inputCustomerDetails(String name, String email, String address, String contact) {
