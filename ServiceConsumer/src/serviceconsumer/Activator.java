5fc4ffb4b3eda35127264387af682222a40fa44f
Udeshika Balasooriya <159142506+UdeshikaP@users.noreply.github.com>
2025-02-27 17:00:13 +0530

    update

--- ServiceConsumer/src/serviceconsumer/Activator.java
+++ ServiceConsumer/src/serviceconsumer/Activator.java
@@ -8,9 +8,8 @@ import serviceproducer.ICustomerService;
 import serviceproducer.IFoodMenu;
 import serviceproducer.IPaymentService;
 
-
 public class Activator implements BundleActivator {
-	
+
 	private ServiceReference foodMenuReServiceReference;
 	private ServiceReference paymentServiceReference;
 
@@ -20,46 +19,47 @@ public class Activator implements BundleActivator {
 	@Override
 	public void start(BundleContext context) throws Exception {
 
+		System.out.println("Consumer services strated....");
+
+		// Get references to the producer services
+		foodMenuReServiceReference = context.getServiceReference(IFoodMenu.class.getName());
+		paymentServiceReference = context.getServiceReference(IPaymentService.class.getName());
 		// Get the service reference for ICustomerService
 		customerServiceReference = context.getServiceReference(ICustomerService.class.getName());
+
+		IFoodMenu foodMenuService = (IFoodMenu) context.getService(foodMenuReServiceReference);
+		IPaymentService paymentService = (IPaymentService) context.getService(paymentServiceReference);
 		// Obtain the actual service object
 		ICustomerService customerService = (ICustomerService) context.getService(customerServiceReference);
+
+		// Initialize consumer services
+
+		PaymentSystem paymentSystem = new PaymentSystem(paymentService, foodMenuService);
 
 		// Create an object of CustomerSystem using the retrieved customerService
 		CustomerSystem customerSystem = new CustomerSystem(customerService);
 
 		// Provide sample customer details
 		customerSystem.inputCustomerDetails("Amali", "amali@gmail.com", "Kurunegala", "0110000000");
+
+		// Sample data
+		paymentSystem.processPayment("Pizza", 2, "Credit Card", foodMenuService, "Credit Card");
+
 		// Provide notification
 
 		customerSystem.notifyCustomer("Order is comfirmed");
- 
-		System.out.println("Consumer services strated....");
-		
-		//Get references to the producer services
-		foodMenuReServiceReference = context.getServiceReference(IFoodMenu.class.getName());
-		paymentServiceReference = context.getServiceReference(IPaymentService.class.getName());
-		
-		IFoodMenu foodMenuService = (IFoodMenu) context.getService(foodMenuReServiceReference);
-		IPaymentService paymentService = (IPaymentService) context.getService(paymentServiceReference);
-	
-		//Initialize consumer services
-		
-		PaymentSystem paymentSystem = new PaymentSystem(paymentService,foodMenuService);
-		
-		//Sample data
-		paymentSystem.processPayment("Pizza", 2, "Credit Card", foodMenuService, "Credit Card");
-	
+
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
 
-		System.out.println(".....Consumer service stopped.....");
-		context.ungetService(customerServiceReference);		
+		System.out.println("Consumer services stopped");
+		context.ungetService(customerServiceReference);
+
 		context.ungetService(foodMenuReServiceReference);
 		context.ungetService(paymentServiceReference);
-		
+
 	}
 
 }
