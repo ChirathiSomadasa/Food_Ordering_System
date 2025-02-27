8bebed78a65717c6f2f756c76892dc4c49479142
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-02-27 15:11:04 +0530

    Update Consumer Activator

--- ServiceConsumer/src/serviceconsumer/OrderProcessorImpl.java
+++ ServiceConsumer/src/serviceconsumer/OrderProcessorImpl.java
@@ -5,19 +5,19 @@ import serviceproducer.IFoodMenu;
 public class OrderProcessorImpl {
 	
 	private IFoodMenu menuService;
-//	private PaymentSystem paymentSystem;
+
 
 	public OrderProcessorImpl(IFoodMenu menuService) {
 		// TODO Auto-generated constructor stub
 		this.menuService = menuService;
-//		this.paymentSystem = paymentSystem;
+
 	}
 	
 	public void placeOrder(String item, int qty) {
 		menuService.displayMenu();
 		System.out.println("Order place : " + item + "x" + qty );
 		
-//		paymentSystem.processPayment(item, qty, paymentMethod, menuService);
+
 	}
 
 }
