ee95e029d81c10f5989c769084481711591931ef
IT22309624 <129936797+IT22309624@users.noreply.github.com>
2025-02-27 07:10:42 +0530

    restaurant menu service
    both producer and consumer parts

---
+++ ServiceProducer/src/serviceproducer/IOrderProcessor.java
@@ -0,0 +1,6 @@
+package serviceproducer;
+
+public interface IOrderProcessor {
+
+	public void processOrder(String item, int qty);
+}
---
+++ ServiceProducer/src/serviceproducer/RestaurantServiceImpl.java
@@ -0,0 +1,21 @@
+package serviceproducer;
+
+public class RestaurantServiceImpl implements IFoodMenu {
+
+	public RestaurantServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String displayMenu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double getPrice(String item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
